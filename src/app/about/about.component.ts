de08ef4b41f327b2490fa97d325a5fd51b3d3081
Max <max.belleville@hotmail.com>
2020-02-11 14:46:12 -0500

    Made responsive

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,5 +11,23 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  overlay:HTMLElement;
+  menu:HTMLElement;
+  header:HTMLElement;
+  toggleOn: boolean =false;
+  onClick() {
+    this.overlay=document.getElementById("overlay");
+    this.menu=document.getElementById("menu");
+    this.header=document.getElementById("header");
+    if(!this.toggleOn){
+      this.toggleOn=true;
+      this.menu.style.display="block";
+      this.overlay.style.height=(document.body.offsetHeight-this.header.offsetHeight)+"px";
+    }
+    else {
+      this.toggleOn=false;
+      this.menu.style.display="none";
+      this.overlay.style.height="0";
+    }
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
   }
 
+  ngOnInit() {
+   
+  }
+  overlay:HTMLElement;
+  main:HTMLElement;
+  menu:HTMLElement;
+  toggleOn: boolean =false;
+  onClick() {
+    this.overlay=document.getElementById("overlay");
+    this.menu=document.getElementById("menu");
+    this.main=document.getElementById("main");
+    if(!this.toggleOn){
+      this.toggleOn=true;
+      this.menu.style.display="block";
+      this.overlay.style.height=(<HTMLElement>this.main.firstElementChild).offsetHeight+"px";
+    }
+    else {
+      this.toggleOn=false;
+      this.menu.style.display="none";
+      this.overlay.style.height="0";
+    }
+  }
 }
